f536a6718bc4cfcdb0be83d4c2f6e2d4e68641e9
darthbel <febelgine@gmail.com>
2019-03-28 16:55:29 -0400

    adding home and get-page

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GetPageComponent } from './get-page/get-page.component';
+import { GetByIdFormComponent } from './get-by-id-form/get-by-id-form.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetPageComponent,
+    GetByIdFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
