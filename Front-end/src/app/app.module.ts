940aed0f2eea41f0a00b1efdc53889b8720c77c5
Humberto Rovina <32313091+BertoRovina@users.noreply.github.com>
2019-03-28 18:51:43 -0400

    Adding first side-nav

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { GetPageComponent } from './get-page/get-page.component';
 import { GetByIdFormComponent } from './get-by-id-form/get-by-id-form.component';
 import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GetPageComponent,
-    GetByIdFormComponent
+    GetByIdFormComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
